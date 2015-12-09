32a1f0b9fb469b022b404259aa101092ea1ec3f1
jxelremo <jxelremo@gmail.com>
2015-12-09 11:00:40 +0000

    Delete HistogramDisplay.java

--- src/kata7/Application/swing/HistogramDisplay.java
+++
@@ -1,8 +0,0 @@
-package kata7.Application.swing;
-
-import kata7.model.Histogram;
-
-public interface HistogramDisplay {
-    public Histogram histogram();
-    public void show (Histogram histogram);
-}
